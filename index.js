dbf81baad0c173b62a9780946191f5b016cab645
Robbie Bardijn <robbie.bardijn@gmail.com>
2020-12-17 15:25:26 +0100

    finish course

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, PubSub } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
+const { Kind } = require("graphql/language");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const movies = [
-  {
-    id: "1",
-    title: "5 Deadly Venoms",
-    releaseDate: "10-10-1983",
-    rating: 5,
-  },
-  {
-    id: "2",
-    title: "36th Chamber",
-    releaseDate: "10-12-1983",
-    rating: 3,
-    actor: [{ id: "2321", name: "Gordon Liu" }],
-  },
-];
+mongoose.connect(
+  "mongodb+srv://robbie:mongo@cluster0.lvduk.mongodb.net/kungfu?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+const movieSchema = new Schema({
+  title: String,
+  releaseDate: Date,
+  rating: Number,
+  status: String,
+  actorIds: [String],
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
 
 const typeDefs = gql`
+  # fragment Meta on Movie {
+  #   releaseDate: Date
+  #   rating: Int
+  # }
+
+  scalar Date
+
   enum Status {
     WATCHED
     INTERESTED
@@ -26,13 +36,13 @@ const typeDefs = gql`
 
   type Actor {
     id: ID!
-    name: String
+    name: String!
   }
 
   type Movie {
     id: ID!
     title: String!
-    releaseDate: String!
+    releaseDate: Date
     rating: Int
     status: Status
     actor: [Actor] # valid: null, [], [...someData]
@@ -46,17 +56,102 @@ const typeDefs = gql`
     movies: [Movie]
     movie(id: ID): Movie
   }
+
+  input ActorInput {
+    id: ID
+  }
+
+  input MovieInput {
+    id: ID
+    title: String
+    releaseDate: Date
+    rating: Int
+    status: Status
+    actor: [ActorInput]
+  }
+
+  type Mutation {
+    addMovie(movie: MovieInput): [Movie]
+  }
+
+  type Subscription {
+    movieAdded: Movie
+  }
 `;
 
+const pubsub = new PubSub();
+const MOVIE_ADDED = "MOVIE_ADDED";
+
 const resolvers = {
-  Query: {
-    movies: () => {
-      return movies;
-    },
-    movie: (obj, { id }, context, info) => {
-      return movies.find((movie) => movie.id === id);
+  Subscription: {
+    movieAdded: {
+      subscribe: () => {
+        return pubsub.asyncIterator([MOVIE_ADDED]);
+      },
     },
   },
+  Query: {
+    movies: async () => {
+      try {
+        return await Movie.find();
+      } catch (e) {
+        console.log(e);
+        return [];
+      }
+    },
+    movie: async (obj, { id }) => {
+      try {
+        return await Movie.findById(id);
+      } catch (e) {
+        console.log(e);
+        return [];
+      }
+    },
+  },
+  Mutation: {
+    addMovie: async (obj, { movie }, { userId }) => {
+      try {
+        if (userId) {
+          const newMovie = await Movie.create({
+            ...movie,
+          });
+          pubsub.publish(MOVIE_ADDED, { movieAdded: newMovie });
+          const allMovies = await Movie.find();
+          return allMovies;
+        }
+        return movies;
+      } catch (e) {
+        console.log(e);
+        return [];
+      }
+    },
+  },
+  Movie: {
+    actor: (obj, arg, context) => {
+      const actorIds = obj.actor.map((actor) => actor.id);
+      const foundActors = actors.filter((actor) => actorIds.includes(actor.id));
+      return foundActors;
+    },
+  },
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "A normal date",
+    parseValue(value) {
+      // value from the client
+
+      return new Date(value);
+    },
+    serialize(value) {
+      // value from the servere
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
 
 const server = new ApolloServer({
@@ -64,8 +159,19 @@ const server = new ApolloServer({
   resolvers,
   introspection: true,
   playground: true,
+  context: ({ req }) => {
+    const fakeUser = {
+      userId: "iamuser",
+    };
+    return { ...fakeUser };
+  },
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server started at ${url}`);
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("CONNECTED");
+
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`Server started at ${url}`);
+  });
 });
---
+++ what-is-an-ast.md
@@ -0,0 +1,15 @@
+# Wat is an AST?
+
+Litterally: Abstract Syntaxt Tree.
+
+- Takes a string, converts it to a represantion that the parser understands. An abstract syntaxt tree.
+- fieldName
+- fieldNodes []
+- returnType
+- parentType (Query, Mutation,...)
+- ...
+- path
+- schema
+- fragments
+
+**gql`` parses your string into an AST**
